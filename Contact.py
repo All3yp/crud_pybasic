ae7277390dcad3375bd30dc08c264083fffd8d50
Alley Pereira <noyallepereira@gmail.com>
2023-08-23 02:46:18 +0000

    update Person class

--- Contact.py
+++
@@ -1,2 +0,0 @@
-class Contact:
-    name, phone, email, address
---
+++ src/Person.py
@@ -0,0 +1,7 @@
+class Person:
+    def __init__(self, name, age, mail, height, is_student):
+    self.name = name 
+    self.age = age
+    self.mail = mail
+    self.height = height
+    self.is_student = is_student
